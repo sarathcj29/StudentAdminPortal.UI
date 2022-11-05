1bd9ae18c3673e8376c123ff15efc3770b9b62df
sarathcj29 <sarathcj29@gmail.com>
2022-11-05 22:52:33 +0530

    Added table, pagination, sorting and search options

--- studentadminportal-ui/src/app/app-routing.module.ts
+++ studentadminportal-ui/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StudentsComponent } from './students/students.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: StudentsComponent,
+  },
+  {
+    path: 'students',
+    component: StudentsComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
